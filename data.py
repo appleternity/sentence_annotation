be5c273b2842c541348b7784a08835a3a626682a
appleternity <b99901034@ntu.edu.tw>
2019-04-09 21:56:01 -0400

    final version

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ import os
 
 from nltk.stem import WordNetLemmatizer
 from interface import tokenize_pos
+from nltk.tree import Tree
 
 def parsing():
     with open("sentences.csv", 'r') as infile:
@@ -30,7 +31,6 @@ def parsing():
 
     with open("sentences.json", 'w', encoding='utf-8') as outfile:
         json.dump(result, outfile, indent=4)
-
 
 def find_target_word_with_pos(stemmer, pos_list, word):
     for index, (w, pos) in enumerate(pos_list):
@@ -72,14 +72,21 @@ def preprocessing():
     with open("sentences_processed.json", 'w', encoding='utf-8') as outfile:
         json.dump(data, outfile, indent=4)
 
-def display_sent(sent, target, another_word):
+def display_sent(sent, target, another_word, word_mapping):
     ori = sent["sentence"].replace(
         sent["tokens"][sent["target_index"]][0],
         "<strong>{}</strong>".format(sent["tokens"][sent["target_index"]][0])
     )
+    
+    t = sent["tokens"][sent["target_index"]][1]
+    try:
+        word = word_mapping[another_word][t]
+    except KeyError:
+        print(another_word, t, sent["tokens"][sent["target_index"]][0])
+        print(sent["sentence"])
     new = sent["sentence"].replace(
         sent["tokens"][sent["target_index"]][0],
-        "<strong>{}</strong>".format(another_word)
+        "<strong>{}</strong>".format(word)
     )
     return ori, new
 
@@ -98,12 +105,15 @@ def generate_html():
     with open("index.html", 'r', encoding='utf-8') as infile:
         template = infile.read()
 
+    with open("word_mapping.json", 'r', encoding='utf-8') as infile:
+        word_mapping = json.load(infile)
+
     count = 0
     for w1, w2 in wordsets:
         # w1
         sent_list = data[w1]
         for sent in sent_list:
-            ori, new = display_sent(sent, w1, w2) 
+            ori, new = display_sent(sent, w1, w2, word_mapping) 
             with open("html/{:0>4}.html".format(count), 'w', encoding='utf-8') as outfile:
                 #outfile.write(template.replace("{{s1}}", ori).replace("{{s2}}", new))
                 outfile.write(generate_template(
@@ -115,7 +125,7 @@ def generate_html():
         # w2
         sent_list = data[w2]
         for sent in sent_list:
-            ori, new = display_sent(sent, w2, w1) 
+            ori, new = display_sent(sent, w2, w1, word_mapping) 
             with open("html/{:0>4}.html".format(count), 'w', encoding='utf-8') as outfile:
                 #outfile.write(template.replace("{{s1}}", ori).replace("{{s2}}", new))
                 outfile.write(generate_template(
@@ -127,10 +137,44 @@ def generate_html():
     # update static
     os.system("cp static/* html/static/")
 
+def generate_word_mapping():
+    with open("wordset.json", 'r', encoding='utf-8') as infile:
+        wordsets = json.load(infile)
+
+    dir_path = "/home/appleternity/workspace/lab/NLPSA/adversarial/data/sentence"
+    stemmer = WordNetLemmatizer()
+    wordlist = [w for wordset in wordsets for w in wordset]
+    word_mapping = {}
+
+    for word in wordlist:
+        print(word)
+        word_mapping[word] = {}
+
+        with open(os.path.join(dir_path, word+".json"), 'r', encoding='utf-8') as infile:
+            data = json.load(infile)
+
+        for sent in data[:500]:
+            sent = Tree.fromstring(sent["parsedTree"])
+            target_index = find_target_word_with_pos(stemmer, sent.pos(), word)
+            if target_index == -1:
+                continue
+            w, t = sent.pos()[target_index]
+            if t not in word_mapping[word]:
+                word_mapping[word][t] = set()
+            word_mapping[word][t].add(w)
+
+    for key, value in word_mapping.items():
+        for t, w_set in value.items():
+            value[t] = "|".join(list(w_set))
+
+    with open("word_mapping.json", 'w', encoding='utf-8') as outfile:
+        json.dump(word_mapping, outfile, indent=4)
+
 def main():
     #parsing()
     #preprocessing()
     generate_html()
+    #generate_word_mapping()
 
 if __name__ == "__main__":
     main()
